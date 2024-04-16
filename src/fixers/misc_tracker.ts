ecb00f7a278e4f90daaee1d0ee8933816d12e542
Ante <AnteIndustrial@users.noreply.github.com>
2024-04-16 20:23:21 +1000

    this now more or less works (ugly code and ui though)

--- src/fixers/misc_tracker.ts
+++ src/fixers/misc_tracker.ts
@@ -1,36 +1,75 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { IMutationAware, IStateAware } from "./base";
 import { SettingsObject } from "../settings";
-import { GameStateController } from "../game_state";
+import { GameState, GameStateController } from "../game_state";
 import { getSingletonByClassName } from "../utils";
-
+import { MSG_TYPE_SAVE_SETTINGS } from "../constants";
+import { sendToServiceWorker } from "../comms";
 
 export class MiscTracker implements IMutationAware, IStateAware {
 
     private displayMiscTracker = true;
-    private miscValues: Map<string, TrackedQuality> = new Map();
+    private miscQualities: Map<string, TrackedQuality> = new Map();
+    private qualityNameAndCategory: Map<string, string> = new Map();
+    private qualityNames: string[] = [];
+    private currentState?: GameState;
+    private settingsString: string | undefined;
+    private currentSettings: SettingsObject | undefined;
 
     constructor() {
-        this.miscValues.set("Striped Delights", new TrackedQuality("Story", 0, 105, "tigerstripes"));
-        //TODO this.miscValues.set("Striped Delights", [0, 176])
-        this.miscValues.set("Silver Horseheads", new TrackedQuality("Story", 0, 176, "horsesilver"));
-        this.miscValues.set("Imperial Legitimacy", new TrackedQuality("Story", 0, 100, "empresscourt"));
-        this.miscValues.set("Time Passing in Office", new TrackedQuality("Progress", 0, 12, "declaim2"));
-        this.miscValues.set("Antique Mystery", new TrackedQuality("Elder", 0, 55, "meteors"));
+        //chrome.storage.local.get(null)
+        //console.log(chrome.storage.local.get(null));
+            //console.log(JSON.stringify(Object.fromEntries(this.miscQualities)));
+            //sendToServiceWorker(MSG_TYPE_SAVE_SETTINGS, { settings: this.settings });
+        /*this.miscQualities.set("Striped Delights", { name: "Striped Delights", category: "Story", currentValue: 0, targetValue: 105, image: "tigerstripes" });
+this.miscQualities.set("Silver Horseheads", { name: "Silver Horseheads", category: "Story", currentValue: 0, targetValue: 176, image: "horsesilver" });
+this.miscQualities.set("Imperial Legitimacy", { name: "Imperial Legitimacy", category: "Story", currentValue: 0, targetValue: 100, image: "empresscourt" });
+this.miscQualities.set("Time Passing in Office", { name: "Time Passing in Office", category: "Progress", currentValue: 0, targetValue: 12, image: "declaim2" });
+this.miscQualities.set("Antique Mystery", { name: "Antique Mystery", category: "Elder", currentValue: 0, targetValue: 55, image: "meteors" });*/
     }
 
     applySettings(settings: SettingsObject): void {
-        ;
+        this.currentSettings = settings;
+        console.log("received settings")
+        console.log(settings)
+        let temp = settings.trackedQualities as string;
+        if (!temp) {
+            temp = "{}";
+        }
+        this.miscQualities = new Map(Object.entries(JSON.parse(temp)));
+        /*this.currentSettings = settings;
+        console.log(2)
+        console.log(settings);
+        console.log(3)
+        let temp = settings.trackedQualities as string;
+        if (!temp) {
+            temp = "{\"Striped Delights\":{\"name\":\"Striped Delights\",\"category\":\"Story\",\"currentValue\":0,\"targetValue\":105,\"image\":\"tigerstripes\"},\"Silver Horseheads\":{\"name\":\"Silver Horseheads\",\"category\":\"Story\",\"currentValue\":0,\"targetValue\":176,\"image\":\"horsesilver\"},\"Imperial Legitimacy\":{\"name\":\"Imperial Legitimacy\",\"category\":\"Story\",\"currentValue\":0,\"targetValue\":100,\"image\":\"empresscourt\"},\"Time Passing in Office\":{\"name\":\"Time Passing in Office\",\"category\":\"Progress\",\"currentValue\":0,\"targetValue\":12,\"image\":\"declaim2\"},\"Antique Mystery\":{\"name\":\"Antique Mystery\",\"category\":\"Elder\",\"currentValue\":0,\"targetValue\":55,\"image\":\"meteors\"}}"
+        }
+        this.miscQualities = new Map(Object.entries(JSON.parse(temp)));
+        console.log(temp);
+        console.log(4)
+        console.log(this.miscQualities);
+        console.log(5)*/
+        //todo temp -> miscQualities
     }
+
+    
 
     linkState(state: GameStateController): void {
         state.onCharacterDataLoaded((g) => {
-            for (const [key, value] of this.miscValues) {
-                const quality = g.getQuality(value.getCategory(), key);
+            this.currentState = g;
+            const unsortedQualityNames: string[] = []
+            for (const quality of g.enumerateQualities()) {
+                this.qualityNameAndCategory.set(quality.name, quality.category);
+                unsortedQualityNames.push(quality.name);
+            }
+            this.qualityNames = unsortedQualityNames.sort(this.stringSorter())
+            for (const [key, value] of this.miscQualities) {
+                const quality = g.getQuality(value.category, key);
                 if (quality) {
-                    this.miscValues.get(key)?.setCurrentValue(quality.level);
+                    value.currentValue = quality.level;
                 } else {
-                    this.miscValues.get(key)?.setCurrentValue(0);
+                    value.currentValue = 0;
                 }
 
                 this.updateTracker(key, quality?.level || 0);
@@ -38,64 +77,47 @@ export class MiscTracker implements IMutationAware, IStateAware {
         });
 
         state.onQualityChanged((state, quality, _previous, current) => {
-            if (this.miscValues.has(quality.name)) {
-                this.miscValues.get(quality.name)?.setCurrentValue(current);
+            if (!this.qualityNames.includes(quality.name)) {
+                this.qualityNames.push(quality.name);
+                this.qualityNames = this.qualityNames.sort(this.stringSorter())
+                this.qualityNameAndCategory.set(quality.name, quality.category);
+
+                const miscSelect = document.getElementById("track-target-name");
+                const option = document.createElement("option");
+                option.value = quality.name;
+                option.text = quality.name;
+                miscSelect?.appendChild(option);
+                
+            }
+            if (this.miscQualities.has(quality.name)) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                this.miscQualities.get(quality.name)!.currentValue = current;
                 this.updateTracker(quality.name, current);
             }
         });
     }
 
-    private createTracker(valueName: string): HTMLElement {
-        const value = this.miscValues.get(valueName);
-        const currentValue = value?.getCurrentValue() || 0;
-        const targetValue = value?.getTargetValue() || 0;
-        const icon = value?.getImage() || "question";
-
-        const newDisplay = this.createTrackerDisplay(valueName, icon + "small", currentValue, targetValue);
-
-        return newDisplay;
+    private stringSorter(): ((a: string, b: string) => number) | undefined {
+        return (s1, s2) => {
+            if (s1 > s2) {
+                return 1;
+            }
+            if (s1 < s2) {
+                return -1;
+            }
+            return 0;
+        };
     }
 
-    private updateTracker(title: string, value: number) {
-        const miscTracker = document.getElementById("misc-tracker");
-        if (!miscTracker) {
-            return;
-        }
+    //create the html element to track one quality
+    private createTracker(title: string): HTMLElement {
+        const trackedQuality = this.miscQualities.get(title);
+        const initialValue = trackedQuality?.currentValue || 0;
+        const targetValue = trackedQuality?.targetValue || 0;
+        const icon = trackedQuality?.image || "question";
 
-        let qualityDisplay = null;
-        const existingDisplays = miscTracker.getElementsByClassName("tracked-misc");
-        for (const display of existingDisplays) {
-            const displayTitle = (display as HTMLElement).dataset.qualityName;
-            if (displayTitle == title) {
-                qualityDisplay = display as HTMLElement;
-                break;
-            }
-        }
-
-        if (!qualityDisplay) {
-            return;
-        }
-
-        const valueSpan = getSingletonByClassName(qualityDisplay, "item__value");
-        const target = this.miscValues.get(title)?.getTargetValue() || 1;
-        if (valueSpan) {
-            valueSpan.textContent = ` ${value} / ${target}`;
-        }
-
-        const progressBarSpan = getSingletonByClassName(qualityDisplay, "progress-bar__stripe") as HTMLElement;
-        if (progressBarSpan) {
-            let percentage = (value / target) * 100;
-            if (percentage > 100) {
-                percentage = 100;
-            }
-            progressBarSpan.style.cssText = `width: ${percentage}%;`;
-        }
-        qualityDisplay.style.cssText = "text-align: left";
-
-    }
-
-    private createTrackerDisplay(title: string, icon: string, initialValue: number, targetValue: number): HTMLElement {
         const li = document.createElement("li");
+        li.id = `${title}-tracker`;
         li.classList.add("js-item", "item", "sidebar-quality", "tracked-misc");
         li.style.cssText = "text-align: left";
         li.dataset.qualityName = title;
@@ -125,7 +147,7 @@ export class MiscTracker implements IMutationAware, IStateAware {
 
         const img = document.createElement("img");
         img.classList.add("cursor-default");
-        img.setAttribute("alt", `Tracking: ${title}`);
+        img.setAttribute("alt", `${title}`);
         img.setAttribute("src", `//images.fallenlondon.com/icons/${icon}.png`);
         img.setAttribute("aria-label", `${title}`);
 
@@ -137,15 +159,95 @@ export class MiscTracker implements IMutationAware, IStateAware {
         }
         span4.style.cssText = `width: ${percentage}%;`;
 
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("buttonlet-container")
+        deleteButton.setAttribute("aria-label", "Stop Tracking");
+        deleteButton.setAttribute("type", "button");
+
+        const deleteSpan1 = document.createElement("span");
+        deleteSpan1.classList.add("buttonlet", "fa-stack", "fa-lg", "buttonlet-enabled", "buttonlet-delete");
+        deleteSpan1.setAttribute("title", "Stop Tracking")
+
+        const deleteSpan2 = document.createElement("span");
+        deleteSpan2.classList.add("fa", "fa-circle", "fa-stack-2x");
+
+        const deleteSpan3 = document.createElement("span");
+        deleteSpan3.classList.add("fa", "fa-inverse", "fa-stack-1x", "fa-times");
+
+        const deleteSpan4 = document.createElement("span");
+        deleteSpan4.classList.add("u-visually-hidden")
+        deleteSpan4.textContent = "delete";
+
+        deleteButton.appendChild(deleteSpan1);
+        deleteSpan1.appendChild(deleteSpan2);
+        deleteSpan1.appendChild(deleteSpan3);
+        deleteSpan1.appendChild(deleteSpan4);
+
+        deleteButton.addEventListener("click", () => {
+            console.log("before delete")
+            console.log(this.miscQualities)
+            console.log(this.currentSettings)
+            document.getElementById(li.id)?.remove();
+            this.miscQualities.delete(title);
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            this.currentSettings!.trackedQualities = JSON.stringify(Object.fromEntries(this.miscQualities))
+
+            sendToServiceWorker(MSG_TYPE_SAVE_SETTINGS, { settings: this.currentSettings });
+
+            console.log(this.miscQualities)
+            console.log(this.currentSettings)
+        })
+
         li.appendChild(div);
         li.appendChild(div3);
+        li.appendChild(deleteButton);
         div.appendChild(div4);
         div3.appendChild(span);
         div3.appendChild(span3);
         div3.appendChild(div5);
         div4.appendChild(img);
         div5.appendChild(span4);
+        
         return li;
+    }
+
+    //find the element, update the number
+    private updateTracker(title: string, value: number) {
+        const miscTracker = document.getElementById("misc-tracker");
+        if (!miscTracker) {
+            return;
+        }
+
+        let qualityDisplay = null;
+        const existingDisplays = miscTracker.getElementsByClassName("tracked-misc");
+        for (const display of existingDisplays) {
+            const displayTitle = (display as HTMLElement).dataset.qualityName;
+            if (displayTitle == title) {
+                qualityDisplay = display as HTMLElement;
+                break;
+            }
+        }
+
+        if (!qualityDisplay) {
+            return;
+        }
+
+        const valueSpan = getSingletonByClassName(qualityDisplay, "item__value");
+        const target = this.miscQualities.get(title)?.targetValue || 1;
+        if (valueSpan) {
+            valueSpan.textContent = ` ${value} / ${target}`;
+        }
+
+        const progressBarSpan = getSingletonByClassName(qualityDisplay, "progress-bar__stripe") as HTMLElement;
+        if (progressBarSpan) {
+            let percentage = (value / target) * 100;
+            if (percentage > 100) {
+                percentage = 100;
+            }
+            progressBarSpan.style.cssText = `width: ${percentage}%;`;
+        }
+        qualityDisplay.style.cssText = "text-align: left";
+
     }
 
     checkEligibility(node: HTMLElement): boolean {
@@ -190,10 +292,58 @@ export class MiscTracker implements IMutationAware, IStateAware {
             miscPanel.classList.add("items", "items--list");
             fragment.appendChild(miscPanel);
 
-            for (const valueName of this.miscValues.keys()) {
+            for (const valueName of this.miscQualities.keys()) {
                 const miscDisplay = this.createTracker(valueName);
                 miscPanel.appendChild(miscDisplay);
             }
+
+            const miscPicker = document.createElement("div");
+            const miscSelect = document.createElement("select");
+            miscSelect.id = "track-target-name";
+            miscPicker.appendChild(miscSelect);
+            for (const qualityName of this.qualityNames) {
+                const option = document.createElement("option");
+                option.value = qualityName;
+                option.text = qualityName;
+                miscSelect.appendChild(option);
+            }
+            const targetInput = document.createElement("input");
+            targetInput.id = "track-target-number";
+            targetInput.type = "number";
+            miscPicker.appendChild(targetInput);
+
+            const trackButton = document.createElement("button");
+            trackButton.classList.add("js-tt", "button", "button--primary", "button--margin", "button--go")
+            trackButton.addEventListener("click", () => {
+                const trackName = (document.getElementById("track-target-name") as HTMLSelectElement).value;
+                const trackNumber = Number((document.getElementById("track-target-number") as HTMLInputElement).value);
+                const trackCategory = this.qualityNameAndCategory.get(trackName) || "";
+                const trackCurrent: number = this.currentState?.getQuality(trackCategory, trackName)?.level || 0;
+                const trackImage: string = this.currentState?.getQuality(trackCategory, trackName)?.image || "question"
+                const newQuality = { name: trackName, category: trackCategory, currentValue: trackCurrent, targetValue: trackNumber, image: trackImage };
+                console.log("before add")
+                console.log(this.miscQualities)
+                console.log(this.currentSettings)
+                this.miscQualities.set(trackName, newQuality);
+                if (this.currentSettings) {
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    this.currentSettings!.trackedQualities = JSON.stringify(Object.fromEntries(this.miscQualities)) ;
+                }
+
+                sendToServiceWorker(MSG_TYPE_SAVE_SETTINGS, { settings: this.currentSettings });
+                console.log(this.miscQualities)
+                console.log(this.currentSettings)
+               
+                const miscDisplay = this.createTracker(trackName);
+                miscPanel?.appendChild(miscDisplay);
+                miscPanel?.appendChild(miscPicker);
+            })
+            const trackText = document.createElement("span");
+            trackText.textContent = "Track";
+            trackButton.appendChild(trackText);
+            miscPicker.appendChild(trackButton);
+
+            miscPanel.appendChild(miscPicker);
 
             sidebar.appendChild(fragment);
         }
@@ -209,36 +359,12 @@ export class MiscTracker implements IMutationAware, IStateAware {
 
 }
 
-class TrackedQuality {
-    private category: string;
-    private currentValue: number;
-    private targetValue: number;
-    private image: string;
 
-    constructor(category: string, currentValue: number, targetValue: number, image: string) {
-        this.category = category;
-        this.currentValue = currentValue;
-        this.targetValue = targetValue;
-        this.image = image;
-    }
 
-    public getCategory(): string {
-        return this.category;
-    }
-
-    public getCurrentValue(): number {
-        return this.currentValue;
-    }
-
-    public setCurrentValue(newValue: number) {
-        this.currentValue = newValue;
-    }
-
-    public getTargetValue(): number {
-        return this.targetValue;
-    }
-
-    public getImage(): string {
-        return this.image;
-    }
-}
+export interface TrackedQuality {
+    name: string;
+    category: string;
+    currentValue: number;
+    targetValue: number;
+    image: string;
+};
